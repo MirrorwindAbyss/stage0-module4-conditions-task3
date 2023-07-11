9102db43d7a6f32c5081c107ed0d50d3603e425c
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-11 13:22:01 +0500

    Done!

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character<'A'||character>'z'){
+            System.out.println("wrong alphabet!");
+        } else if(character=='A'||character=='E'||character=='I'||character=='O'||character=='U'||character=='a'||character=='e'||character=='i'||character=='o'||character=='u'){
+            System.out.println("Vowel");
+        } else{
+            System.out.println("Consonant");
+        }
     }
 }
